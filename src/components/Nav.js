5ce6e6666068d56e6d01e906d8d19d4c063bc244
Maria <std145289@ac.eap.gr>
2020-11-04 20:59:51 +0200

    Created react configuration.

---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => (
+    <nav>
+        <li><a>Home</a></li>
+        <li><a>About</a></li>
+        <li><a>Bleh</a></li>
+    </nav>
+);
+
+export default Nav;
